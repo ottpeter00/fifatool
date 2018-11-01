924147b69cbd9efb91d35b824dd9de332df1b8c3
ottpeter00 <44669369+ottpeter00@users.noreply.github.com>
2018-11-01 13:00:05 +0100

    Verschiebung

---
+++ FUTool/src/WINDOW.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class WINDOW {
+	
+	private JFrame fenster;
+	
+	
+	
+	public WINDOW() {
+		
+		fenster = new JFrame("FIFA Ultimate Tool");
+		fenster.setBounds(100,100,600,880);
+		fenster.setVisible(true);
+		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		
+	}
+	
+}
